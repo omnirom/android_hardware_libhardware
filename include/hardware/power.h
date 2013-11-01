e28205dc0ee1a5236913538e6292d4224e94da1b
Steve Kondik <steve@cyngn.com>
2013-11-01 20:28:10 +0000

    power: Add new power hints
     * New hint to support CPU boosting with a duration.
    
    libhardware: Add audio power hint
    
    libhardware: Add power hint to set profile
    
     * A PowerHAL can implement support for this hint to receive
       power profile changes from the framework.
    
    libhardware: Add POWER_HINT_LAUNCH_BOOST for PowerHAL
    
    hardware: Add new getFeature() function to Power HAL
    
     * The current use case for this is to query the power HAL for
       the number of supported profiles.
    
    hardware: Add definition for launch boost info
    
    Conflicts:
    	include/hardware/power.h
    
    Change-Id: If2492b37fef27211826de26b9d510dc3b348a164

--- include/hardware/power.h
+++ include/hardware/power.h
@@ -66,12 +66,16 @@ typedef enum {
     POWER_HINT_VR_MODE = 0x00000007,
     POWER_HINT_LAUNCH = 0x00000008,
     POWER_HINT_DISABLE_TOUCH = 0x00000009,
-    POWER_HINT_CPU_BOOST = 0x00000010,
-    POWER_HINT_POWER_PROFILE = 0x00000100
+    POWER_HINT_CPU_BOOST    = 0x00000010,
+    POWER_HINT_LAUNCH_BOOST = 0x00000011,
+    POWER_HINT_AUDIO        = 0x00000020,
+    POWER_HINT_SET_PROFILE  = 0x00000030
+
 } power_hint_t;
 
 typedef enum {
-    POWER_FEATURE_DOUBLE_TAP_TO_WAKE = 0x00000001
+    POWER_FEATURE_DOUBLE_TAP_TO_WAKE = 0x00000001,
+    POWER_FEATURE_SUPPORTED_PROFILES = 0x00001000
 } feature_t;
 
 /*
@@ -158,6 +162,15 @@ typedef struct {
      */
     power_state_voter_t *voters;
 } power_state_platform_sleep_state_t;
+
+/**
+ * Process info, passed as an opaque handle when
+ * using POWER_HINT_LAUNCH_BOOST.
+ */
+typedef struct launch_boost_info {
+    pid_t pid;
+    const char* packageName;
+} launch_boost_info_t;
 
 /**
  * Every hardware module must have a data structure named HAL_MODULE_INFO_SYM
@@ -258,6 +271,11 @@ typedef struct power_module {
      *     Touch controller could be disabled in those modes to save power.
      *     The data parameter is non-zero when touch could be disabled, and zero
      *     when touch needs to be re-enabled.
+     * POWER_HINT_CPU_BOOST
+     *
+     *     An operation is happening where it would be ideal for the CPU to
+     *     be boosted for a specific duration. The data parameter is an
+     *     integer value of the boost duration in microseconds.
      *
      * A particular platform may choose to ignore any hint.
      *
@@ -280,6 +298,12 @@ typedef struct power_module {
      *
      */
     void (*setFeature)(struct power_module *module, feature_t feature, int state);
+
+    /*
+     * (*getFeature) is called to get the current value of a particular
+     * feature or capability from the hardware or PowerHAL
+     */
+    int (*getFeature)(struct power_module *module, feature_t feature);
 
     /*
      * Platform-level sleep state stats:
@@ -338,7 +362,6 @@ typedef struct power_module {
     int (*get_voter_list)(struct power_module *module, size_t *voter);
 
 } power_module_t;
-
 
 __END_DECLS
 

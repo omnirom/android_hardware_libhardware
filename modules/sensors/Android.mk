29b9cce85c34ef879720d0154a0b3cfd468d02ee
maxwen <max.weninger@gmail.com>
2017-09-28 03:42:42 +0200

    Sensors: MultiHal: add back target sensors.$(TARGET_BOARD_PLATFORM)
    some devices rely on this target
    
    Change-Id: I1581ae60731f30db9230f846cac18bdebd02f1b1

--- modules/sensors/Android.mk
+++ modules/sensors/Android.mk
@@ -41,6 +41,29 @@ LOCAL_STRIP_MODULE := false
 
 include $(BUILD_SHARED_LIBRARY)
 
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := sensors.$(TARGET_BOARD_PLATFORM)
+
+LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_PROPRIETARY_MODULE := true
+
+LOCAL_CFLAGS := -DLOG_TAG=\"MultiHal\"
+
+LOCAL_SRC_FILES := \
+    multihal.cpp \
+    SensorEventQueue.cpp \
+
+LOCAL_SHARED_LIBRARIES := \
+    libcutils \
+    libdl \
+    liblog \
+    libutils \
+
+LOCAL_STRIP_MODULE := false
+
+include $(BUILD_SHARED_LIBRARY)
+
 endif # USE_SENSOR_MULTI_HAL
 
 include $(call all-makefiles-under, $(LOCAL_PATH))
